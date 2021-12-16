d1c302c3c07e911f29c31a39fb9edf570c2f0529
user <tungs@users.noreply.github.com>
2021-12-15 20:46:03 -0800

    removed the realtime and internal functions from the global scope

--- src/overwrite-time.js
+++ src/overwrite-time.js
@@ -12,18 +12,6 @@ import { realtimeDate, realtimeSetTimeout, realtimeRequestAnimationFrame, realti
 
 initializeMediaHandler();
 
-// keeping overwritten objects...
-exportObject._timeweb_oldDate = realtimeDate;
-exportObject._timeweb_oldSetTimeout = realtimeSetTimeout;
-exportObject._timeweb_oldRequestAnimationFrame = realtimeRequestAnimationFrame;
-exportObject._timeweb_oldSetInterval = realtimeSetInterval;
-exportObject._timeweb_oldCancelAnimationFrame = realtimeCancelAnimationFrame;
-exportObject._timeweb_oldClearTimeout = realtimeClearTimeout;
-exportObject._timeweb_oldClearInterval = realtimeClearInterval;
-exportObject._timeweb_oldPerformance = realtimePerformance;
-exportDocument._timeweb_oldCreateElement = realtimeCreateElement;
-exportDocument._timeweb_oldCreateElementNS = realtimeCreateElementNS;
-
 // overwriting built-in functions...
 exportObject.Date = _Date;
 exportObject.performance.now = virtualNow;
@@ -35,10 +23,3 @@ exportObject.clearTimeout = _clearTimeout;
 exportObject.clearInterval = _clearTimeout;
 exportDocument.createElement = _createElement;
 exportDocument.createElementNS = _createElementNS;
-
-// exported custom functions
-exportObject._timeweb_processNextBlock = processNextBlock;
-exportObject._timeweb_processUntilTime = processUntilTime;
-exportObject._timeweb_runAnimationFrames = runAnimationFrames;
-exportObject._timeweb_addFramePreparer = addFramePreparer;
-exportObject._timeweb_runFramePreparers = runFramePreparers;
