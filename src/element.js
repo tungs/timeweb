553e6bfe51e7ccb25519977a597f8a65dff79a1d
user <tungs@users.noreply.github.com>
2021-12-15 23:13:20 -0800

    renamed element processed property

--- src/element.js
+++ src/element.js
@@ -1,4 +1,4 @@
-const processedProperty = '_timeweb_customized';
+const processedProperty = '_timeweb_processed';
 const realtimeProperty = '_timeweb_realtime';
 
 export function markAsProcessed(element, processed = true) {
