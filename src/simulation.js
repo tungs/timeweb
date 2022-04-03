b0b68b507dfd59f7adb1647fd53be47dda8bef9f
user <tungs@users.noreply.github.com>
2022-04-03 01:41:38 -0700

    prelminary simulation for realtime

---
+++ src/simulation.js
@@ -0,0 +1,26 @@
+import { realtimeCancelAnimationFrame, realtimeRequestAnimationFrame, realtimePerformance } from './realtime.js';
+import { virtualNow } from './shared.js';
+import { goTo } from './index.js';
+
+var simulationAnimationId;
+export function startRealtimeSimulation({ fixedFrameRate } = {}) {
+  realtimeCancelAnimationFrame(simulationAnimationId);
+  var simulationStartTime = realtimePerformance.now();
+  var simulationTime = virtualNow();
+  function simulate() {
+    realtimeRequestAnimationFrame(simulate);
+    var frameDelta;
+    if (fixedFrameRate) {
+      frameDelta = fixedFrameRate;
+    } else {
+      frameDelta = realtimePerformance.now() - simulationStartTime();
+    }
+    simulationTime += frameDelta;
+    goTo(simulationTime);
+  }
+  realtimeRequestAnimationFrame(simulate);
+}
+
+export function stopRealtimeSimulation() {
+  realtimeCancelAnimationFrame(simulationAnimationId);
+}
