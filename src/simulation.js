6c9e964ecdc05e2d3fe6e08606d1719405bf2579
user <tungs@users.noreply.github.com>
2023-02-21 05:32:20 -0800

    fix for some realtime simulation edge cases

--- src/simulation.js
+++ src/simulation.js
@@ -2,11 +2,13 @@ import { realtimeCancelAnimationFrame, realtimeRequestAnimationFrame, realtimePe
 import { virtualNow } from './shared.js';
 import { goTo } from './go-to.js';
 
-var simulationAnimationId;
+var simulation;
 export function startRealtimeSimulation({ fixedFrameDuration, requestNextFrameImmediately } = {}) {
   stopRealtimeSimulation();
   var simulationTime = virtualNow();
   var simulationStartTime = realtimePerformance.now() - simulationTime;
+  var running = true;
+  var simulationAnimationId;
   function simulate() {
     if (fixedFrameDuration) {
       simulationTime += fixedFrameDuration;
@@ -14,17 +16,30 @@ export function startRealtimeSimulation({ fixedFrameDuration, requestNextFrameIm
       simulationTime = realtimePerformance.now() - simulationStartTime;
     }
     if (requestNextFrameImmediately) {
-      simulationAnimationId = realtimeRequestAnimationFrame(simulate);
+      if (running) {
+        simulationAnimationId = realtimeRequestAnimationFrame(simulate);
+      }
       goTo(simulationTime);
     } else {
       goTo(simulationTime).then(function () {
-        simulationAnimationId = realtimeRequestAnimationFrame(simulate);
+        if (running) {
+          simulationAnimationId = realtimeRequestAnimationFrame(simulate);
+        }
       });
     }
   }
+  simulation = {
+    stop: function () {
+      running = false;
+      realtimeCancelAnimationFrame(simulationAnimationId);
+      simulation = null;
+    }
+  };
   simulationAnimationId = realtimeRequestAnimationFrame(simulate);
 }
 
 export function stopRealtimeSimulation() {
-  realtimeCancelAnimationFrame(simulationAnimationId);
+  if (simulation) {
+    simulation.stop();
+  }
 }
