b5b5180d248128a31f0d139b0141ba059fe2672c
user <tungs@users.noreply.github.com>
2023-04-25 02:31:32 -0400

    added types for `css-transitions-and-animations.ts`

--- src/css-transitions-and-animations.ts
+++ src/css-transitions-and-animations.ts
@@ -3,19 +3,19 @@ import { exportDocument } from './shared';
 
 export function initializeCSSHandler() {
   initializeAnimationClassHandler();
-  function listener(event) {
+  function listener(event: TransitionEvent | AnimationEvent ) {
     processElementAnimations(event.target);
   }
   function init() {
     processDocumentAnimations();
-    exportDocument.body.addEventListener('transitionrun', listener);
+    exportDocument!.body.addEventListener('transitionrun', listener);
     // 'animationstart' is only dispatched after animation-delay, so it doesn't
     // fully cover getting all animations from CSS animations immediately
     // there doesn't seem to be an equivalent of 'transitionrun' for CSS animations
-    exportDocument.body.addEventListener('animationstart', listener);
+    exportDocument!.body.addEventListener('animationstart', listener);
   }
-  if (!exportDocument.body) {
-    exportDocument.addEventListener('load', init);
+  if (!exportDocument!.body) {
+    exportDocument!.addEventListener('load', init);
   } else {
     init();
   }
