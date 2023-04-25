54de59082b1c0b78a239fa0abcf3472b1802c395
user <tungs@users.noreply.github.com>
2023-04-25 01:25:29 -0400

    adds types to `settings.ts`

--- src/settings.ts
+++ src/settings.ts
@@ -1,10 +1,10 @@
 import { exportObject } from './shared';
 import { logWarning } from './logging';
 var settingsPropertyName = 'timewebConfig';
-var userSettings = {};
-var settings = {};
+var userSettings: { [key: string]: any } = {};
+var settings: { [key: string]: SettingsType<any> } = {};
 
-function isOverwritable(obj) {
+function isOverwritable(obj: any) {
   return obj === undefined || obj === null;
 }
 
@@ -12,13 +12,13 @@ export function importGlobalSettings() {
   setUserSettings(exportObject[settingsPropertyName]);
 }
 
-function verifySettings(settingsToVerify) {
+function verifySettings(settingsToVerify: { [key: string]: unknown }) {
   Object.keys(settingsToVerify).forEach(function (key) {
     if (settings[key] === undefined) {
       logWarning('Unknown user-defined config: ' + key);
     } else {
       if (settings[key].validateFn) {
-        let validationMessage = settings[key].validateFn(settingsToVerify[key]);
+        let validationMessage = settings[key].validateFn!(settingsToVerify[key]);
         if (validationMessage) {
           logWarning(validationMessage);
         }
@@ -27,24 +27,30 @@ function verifySettings(settingsToVerify) {
   });
 }
 
-export function addSetting({ name, defaultValue, validateFn, update, onUpdate }) {
+export interface SettingsType<Type> {
+  name: string;
+  defaultValue: Type;
+  update?(newValue: Type, oldValue: Type): Type;
+  validateFn?(toValidate: any): string | undefined | null;
+  onUpdate?(newValue: Type): unknown;
+}
+export function addSetting<Type>({ name, defaultValue, validateFn, update, onUpdate }: SettingsType<Type>) {
   if (isOverwritable(userSettings[name])) {
     userSettings[name] = defaultValue;
   }
-  settings[name] = { defaultValue, validateFn, update, onUpdate };
+  settings[name] = { name, defaultValue, validateFn, update, onUpdate };
   return {
-    getValue() {
+    getValue(): Type {
       return getSetting(name);
     }
   };
 }
 
-export function getSetting(name) {
+export function getSetting(name: string) {
   return userSettings[name];
 }
 
-export function setUserSettings(config) {
-  config = config || {};
+export function setUserSettings(config: { [key: string]: any } = {}) {
   verifySettings(config);
   Object.keys(config).forEach(function (key) {
     var previousValue = userSettings[key];
@@ -52,7 +58,7 @@ export function setUserSettings(config) {
       return;
     }
     if (settings[key].update) {
-      userSettings[key] = settings[key].update(config[key], userSettings[key]);
+      userSettings[key] = settings[key].update!(config[key], userSettings[key]);
     } else {
       userSettings[key] = config[key];
     }
@@ -60,7 +66,7 @@ export function setUserSettings(config) {
       userSettings[key] = settings[key].defaultValue;
     }
     if (settings[key].onUpdate && previousValue !== userSettings[key]) {
-      settings[key].onUpdate(userSettings[key]);
+      settings[key].onUpdate!(userSettings[key]);
     }
   });
 }
