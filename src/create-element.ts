e0b3feaa7eabb8a946f51f8322d19749bf08b9cf
user <tungs@users.noreply.github.com>
2023-04-25 01:08:30 -0400

    added types to `create-element.ts`

--- src/create-element.ts
+++ src/create-element.ts
@@ -1,35 +1,39 @@
 import { exportDocument } from './shared';
 
-var elementCreateListeners = [];
-var elementNSCreateListeners = [];
+export type ElementCreateListener = (element: HTMLElement, name: string) => unknown;
+export type ElementNSCreateListener = (element: Element, name: string) => unknown;
+var elementCreateListeners: ElementCreateListener[] = [];
+var elementNSCreateListeners: ElementNSCreateListener[] = [];
 
-// TODO: merge references to oldCreateElement(NS) with `realtime.js`
-var oldCreateElement, oldCreateElementNS;
+// TODO: merge references to oldCreateElement(NS) with `realtime.ts`
+type CreateElementType = (this: Document, tagName: string, options?: ElementCreationOptions) => HTMLElement;
+type CreateElementNSType = (this: Document, ns: string | null, tagName: string, options?: ElementCreationOptions | string) => Element;
+
+var oldCreateElement: CreateElementType | undefined;
+var oldCreateElementNS: CreateElementNSType | undefined;
 if (exportDocument) {
   oldCreateElement = exportDocument.createElement;
   oldCreateElementNS = exportDocument.createElementNS;
 }
-
-export function virtualCreateElement(tagName, options) {
-  var element = oldCreateElement.call(exportDocument, tagName, options);
+export function virtualCreateElement(tagName: string, options?: ElementCreationOptions | string): HTMLElement {
+  var element: HTMLElement = (oldCreateElement as CreateElementType).call(exportDocument, tagName, options);
   elementCreateListeners.forEach(function (listener) {
     listener(element, tagName);
   });
   return element;
 }
 
-export function addElementCreateListener(listener) {
+export function addElementCreateListener(listener: ElementCreateListener) {
   elementCreateListeners.push(listener);
 }
-
-export function virtualCreateElementNS(ns, qualifiedName, options) {
-  var element = oldCreateElementNS.call(exportDocument, ns, qualifiedName, options);
+export function virtualCreateElementNS(ns: string | null, qualifiedName: string, options?: ElementCreationOptions | string) {
+  var element = (oldCreateElementNS as CreateElementNSType).call(exportDocument, ns, qualifiedName, options);
   elementNSCreateListeners.forEach(function (listener) {
     listener(element, qualifiedName);
   });
   return element;
 }
 
-export function addElementNSCreateListener(listener) {
+export function addElementNSCreateListener(listener: ElementNSCreateListener) {
   elementNSCreateListeners.push(listener);
 }
