d76cff449c4fab91ee0672002fcfa9dbc805cb2c
user <tungs@users.noreply.github.com>
2023-04-25 00:35:36 -0400

    adds types for `markings.ts`

--- src/markings.ts
+++ src/markings.ts
@@ -1,15 +1,15 @@
 const processedProperty = '_timeweb_processed';
 const realtimeProperty = '_timeweb_realtime';
 
-export function markAsProcessed(element, processed = true) {
+export function markAsProcessed(element: any, processed = true) {
   element[processedProperty] = processed;
 }
 
-export function markAsRealtime(element, realtime = true) {
+export function markAsRealtime(element: any, realtime = true) {
   element[realtimeProperty] = realtime;
 }
 
-export function shouldBeProcessed(element) {
+export function shouldBeProcessed(element: any) {
   if (element.dataset && element.dataset.timewebRealtime !== undefined) {
     return false;
   }
