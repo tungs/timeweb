f5e2a0cd10f484423555ad546a73d39af572ebc3
user <tungs@users.noreply.github.com>
2023-04-25 01:32:37 -0400

    add types to `realtime.ts`

--- src/realtime.ts
+++ src/realtime.ts
@@ -6,35 +6,36 @@ import { exportObject, exportDocument } from './shared';
 import { markAsRealtime } from './markings';
 export let realtimeDate = exportObject.Date;
 export let realtimeCustomEvent = exportObject.CustomEvent;
-export let realtimeSetTimeout = exportObject.setTimeout.bind(exportObject);
-export let realtimeRequestAnimationFrame = exportObject.requestAnimationFrame.bind(exportObject);
-export let realtimeSetInterval = exportObject.setInterval.bind(exportObject);
-export let realtimeCancelAnimationFrame = exportObject.cancelAnimationFrame.bind(exportObject);
-export let realtimeClearTimeout = exportObject.clearTimeout.bind(exportObject);
-export let realtimeClearInterval = exportObject.clearInterval.bind(exportObject);
+export let realtimeSetTimeout: typeof exportObject.setTimeout = exportObject.setTimeout.bind(exportObject);
+export let realtimeRequestAnimationFrame: typeof exportObject.requestAnimationFrame = exportObject.requestAnimationFrame.bind(exportObject);
+export let realtimeSetInterval: typeof exportObject.setInterval = exportObject.setInterval.bind(exportObject);
+export let realtimeCancelAnimationFrame: typeof exportObject.cancelAnimationFrame = exportObject.cancelAnimationFrame.bind(exportObject);
+export let realtimeClearTimeout: typeof exportObject.clearTimeout = exportObject.clearTimeout.bind(exportObject);
+export let realtimeClearInterval: typeof exportObject.clearInterval = exportObject.clearInterval.bind(exportObject);
 let oldPerformanceNow = exportObject.performance.now;
 let oldPerformance = exportObject.performance;
 // performance.now() requires performance to be the caller
 export let realtimePerformance = {
-  now: oldPerformanceNow.bind(oldPerformance)
+  now: oldPerformanceNow.bind(oldPerformance) as () => number
 };
-let oldCreateElement, oldCreateElementNS;
+let oldCreateElement: undefined | typeof document.createElement;
+let oldCreateElementNS: undefined | typeof document.createElementNS;
 if (exportDocument) {
   oldCreateElement = exportDocument.createElement;
   oldCreateElementNS = exportDocument.createElementNS;
 }
 
 export let realtimeCreateElement = !exportDocument ? undefined : function () {
-  let element = oldCreateElement.apply(exportDocument, arguments);
+  let element = oldCreateElement!.apply(exportDocument, arguments);
   markAsRealtime(element);
   return element;
-};
+} as typeof exportDocument.createElement;
 
 export let realtimeCreateElementNS = !exportDocument ? undefined : function () {
-  let element = oldCreateElementNS.apply(exportDocument, arguments);
+  let element = oldCreateElementNS!.apply(exportDocument, arguments);
   markAsRealtime(element);
   return element;
-};
+} as typeof exportDocument.createElementNS;
 
 export let realtime = {
   Date: realtimeDate,
